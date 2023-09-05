3e2c07f92721662cd45c44fad3c2d3ac0fd17e4b
= <=>
2023-09-05 23:20:16 +0100

    agencies

--- add_agency.php
+++ add_agency.php
@@ -1,17 +1,32 @@
  <?php
- if (isset($_GET['id'])) {
-     $id = $_GET['id'];
- } else {
-     $id = 2;
+ 
+ include 'inc/db.php';
+ include 'inc/top-menu.php';
+ $page = 'agencies';
+ include 'inc/aside.php';
+ 
+ if (isset($_POST) and $_SERVER['REQUEST_METHOD'] == 'POST') {
+     $agency_name = $_POST['agency_name'];
+     $phone = $_POST['phone'];
+     $email = $_POST['email'];
+     $address = $_POST['address'];
+     $state = $_POST['state'];
+     $fullname = $_POST['fullname'];
+ 
+     if (!empty($agency_name) and !empty($state) and !empty($fullname)) {
+         $sql = "INSERT INTO `tblagencies`(`name`, `state`, `contact_person`, `phone`, `email`, `address`) VALUES
+          ('$agency_name','$state','$fullname','$phone','$email', '$address')";
+         $result = mysqli_query($dbc, $sql);
+         if (mysqli_affected_rows($dbc) == 1) {
+             $message = 'Record Added Successfully';
+             $alert = 'alert alert-info alert-dismissible';
+         } else {
+             $message = 'Something went wrong, try again';
+             $alert = 'alert alert-danger alert-dismissible';
+         }
+     }
  }
  
- include 'inc/top-menu.php'; ?>
-
-
- <!-- Main Sidebar Container -->
- <?php
- $page = 'director';
- include 'inc/aside.php';
  ?>
 
  <!-- Content Wrapper. Contains page content -->
@@ -37,86 +52,112 @@
              <!-- /.row -->
              <!-- Main row -->
              <div class="row">
-             <div class="col-sm-12">
-                 <div class="card card-primary">
-                     <div class="card-header">
-                         <h3 class="card-title">Add User Form</h3>
-                     </div>
-                     <!-- /.card-header -->
-                     <!-- form start -->
-                     <form>
-                         <div class="card-body">
-                             <div class="row">
-                                 <div class="col-sm-6">
-                                     <div class="form-group">
-                                         <label for="exampleInputFullName">Full Name</label>
+                 <div class="col-sm-12">
+                 <?php
+                     
+                     if (!empty($message)) {
+                         echo '<div style="width:100%; margin-left:0%">
+                                                                  <div class="' .
+                             $alert .
+                             '">
+                                                                      <button type="button" class="close" data-dismiss="alert"
+                                                                          aria-hidden="true">&times;</button>
+                                                                      <h5><i class="icon fas fa-info"></i> Alert!</h5>
+                                                                      ' .
+                             $message .
+                             '
+                                                                  </div>
+                                                              </div>';
+                     }
+                     
+                     ?>
+                     <div class="card card-primary">
+                         <div class="card-header">
+                             <h3 class="card-title">Add Agency Form</h3>
+                         </div>
+                         <!-- /.card-header -->
+                         <!-- form start -->
+                         <form method="POST" action="add_agency.php" name="add_agency">
+                             <div class="card-body">
+                                 <div class="row">
+                                     <div class="col-sm-6">
+                                         <div class="form-group">
+                                             <label for="exampleInputFullName">Agency Name</label>
+                                             <input type="text" class="form-control" id="exampleInputEmail1"
+                                                 placeholder="Enter Agency Name" name='agency_name'>
+                                         </div>
+                                     </div>
+                                     <div class="col-sm-6">
+                                         <div class="form-group">
+                                             <label for="exampleInputPassword1">Contact Person</label>
+                                             <input type="text" class="form-control" id="exampleInputPassword1"
+                                                 placeholder="Enter Contact Person" name="fullname">
+                                         </div>
+                                     </div>
+
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-sm-6">
+                                         <div class="form-group">
+                                             <label for="exampleInputPassword1">Phone Number</label>
+                                             <input type="text" class="form-control" id="exampleInputPassword1"
+                                                 placeholder="Enter Phone Number" name='phone'>
+                                         </div>
+                                     </div>
+                                     <div class="col-sm-6">
+                                         <div class="form-group">
+                                             <label for="exampleInputEmail1">Email address</label>
+                                             <input type="email" class="form-control" id="exampleInputEmail1"
+                                                 placeholder="Enter email" name='email'>
+                                         </div>
+                                     </div>
+
+
+                                 </div>
+
+
+                                 <div class="row">
+
+                                     <div class="col-sm-6">
+                                            <div class="form-group">
+                                                    <label for="exampleInputPassword1"> State </label>
+                                                    <select id="" class="form-control" name="state">
+                                                        <option value="kano">kano</option>
+                                                        <option value="Jigawa">Jigawa</option>
+                                                        <option value="Katsina">Katsina</option>
+                                                        <option value="Kaduna">Kaduna</option>
+                                                        
+
+
+                                                    </select>
+
+                                            </div>
+                                     </div>
+
+                                     <div class="col-sm-6">
+                                         <label for="">Address</label><br>
                                          <input type="text" class="form-control" id="exampleInputEmail1"
-                                             placeholder="Enter Full Name">
+                                             placeholder="Enter Address" name='address'>
                                      </div>
+
                                  </div>
 
-                                 <div class="col-sm-6">
-                                     <div class="form-group">
-                                         <label for="exampleInputEmail1">Email address</label>
-                                         <input type="email" class="form-control" id="exampleInputEmail1"
-                                             placeholder="Enter email">
-                                     </div>
-                                 </div>
+
                              </div>
-                             <div class="row">
-                                 <div class="col-sm-6">
-                                     <div class="form-group">
-                                         <label for="exampleInputPassword1">Phone Number</label>
-                                         <input type="text" class="form-control" id="exampleInputPassword1"
-                                             placeholder="Enter Number">
-                                     </div>
-                                 </div>
+                     </div>
+                     <!-- /.card-body -->
 
-                                 <div class="col-sm-6">
-                                     <div class="form-group">
-                                         <label for="exampleInputPassword1"> Role </label>
-                                         <select name="role" id="" class="form-control">
-                                             <option value="">Admin</option>
-                                             <option value="">Coordinator</option>
-                                             <option value="">Director</option>
-                                             <option value="">Management</option>
-                                             <option value="">SA</option>
-                                             <option value="">TA</option>
-
-
-                                         </select>
-
-                                     </div>
-                                 </div>
-                             </div>
-
-                             <div class="row">
-                                 <div class="col-sm-12">
-                                     <label for="">Status</label><br>
-                                     <input type="checkbox" name="my-checkbox" checked data-bootstrap-switch
-                                         data-off-color="danger" data-on-color="success">
-                                 </div>
-
-                                 <div class="col-sm-6">
-                                 </div>
-                             </div>
-
-
-
-                         </div>
-                         <!-- /.card-body -->
-
-                         <div class="card-footer">
-                             <button type="submit" class="btn btn-primary">Submit</button>
-                         </div>
+                     <div class="card-footer">
+                         <button type="submit" class="btn btn-primary">Submit</button>
+                     </div>
                      </form>
                  </div>
-</div>
              </div>
-             <!-- /.row (main row) -->
-         </div><!-- /.container-fluid -->
-     </section>
-     <!-- /.content -->
+         </div>
+         <!-- /.row (main row) -->
+ </div><!-- /.container-fluid -->
+ </section>
+ <!-- /.content -->
  </div>
  <!-- /.content-wrapper -->
  <?php include 'inc/footer.php'; ?>
--- agencies.php
+++ agencies.php
@@ -1,5 +1,6 @@
  <?php
  
+ include 'inc/db.php';
  
  include 'inc/top-menu.php'; ?>
 
@@ -18,7 +19,7 @@
              <div class="row mb-2">
                  <div class="col-sm-12">
                      <h1 class="m-0">Agencies</h1>
-                     
+
                  </div><!-- /.col -->
 
              </div><!-- /.row -->
@@ -34,88 +35,77 @@
              <!-- Main row -->
              <div class="row">
                  <div class="col-sm-12">
-                    
 
 
 
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">DataTable with default features</h3> <button type="button" class="btn btn-primary float-right"><i class="fas fa-plus"></i><a href="add_agency.php" style="color:white;"> Add Agency</a></button>
-    </div>
-    <!-- /.card-header -->
-    <div class="card-body">
-        <table id="example1" class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>S/N</th>
-                    <th>Full Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Status</th>
-                    <th>Reset Passord</th>
-                   
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Internet
-                        Explorer 4.0
-                    </td>
-                    <td>Win 95+</td>
-                    <td> 4</td>
-                    <td><input type="checkbox"  name="my-checkbox" checked data-bootstrap-switch data-off-color="danger" data-on-color="success"></td>
-                    <td><input type="submit"  class="btn btn-success btn-sm" value="Reset"></td>
-                </tr>
-                
-                <tr>
-                    <td>13</td>
-                    <td>Netscape 7.2</td>
-                    <td>Win 95+ / Mac OS 8.6-9.2</td>
-                    <td>1.7</td>
-                    <td><input type="checkbox"  name="my-checkbox" checked data-bootstrap-switch data-off-color="danger" data-on-color="success"></td>
-                    <td><input type="submit"  class="btn btn-success btn-sm" value="Reset"></td>
-                </tr>
-                <tr>
-                    <td>Gecko</td>
-                    <td>Netscape Browser 8</td>
-                    <td>Win 98SE+</td>
-                    <td>1.7</td>
-                    <td><input type="checkbox"  name="my-checkbox" checked data-bootstrap-switch data-off-color="danger" data-on-color="success"></td>
-                    <td><input type="submit"  class="btn btn-success btn-sm" value="Reset"></td>
-                </tr>
-                <tr>
-                    <td>Gecko</td>
-                    <td>Netscape Navigator 9</td>
-                    <td>Win 98+ / OSX.2+</td>
-                    <td>1.8</td>
-                    <td>A</td>
-                    <td>A</td>
-                </tr>
-                <tr>
-                    <td>Gecko</td>
-                    <td>Mozilla 1.0</td>
-                    <td>Win 95+ / OSX.1+</td>
-                    <td>1</td>
-                    <td>A</td>
-                    <td>A</td>
-                </tr>
-               
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th>S/N</th>
-                    <th>Full Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Status</th>
-                    <th>Reset Passord</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
-    <!-- /.card-body -->
-</div>
+
+                     <div class="card">
+                         <div class="card-header">
+                             <h3 class="card-title">List of Agencies</h3> <button type="button"
+                                 class="btn btn-primary float-right"><i class="fas fa-plus"></i><a href="add_agency.php"
+                                     style="color:white;"> Add Agency</a></button>
+                         </div>
+                         <!-- /.card-header -->
+                         <div class="card-body">
+                             <table id="example1" class="table table-bordered table-striped">
+                                 <thead>
+                                     <tr>
+                                         <th>S/N</th>
+                                         <th>Agency Name</th>
+                                         <th>State</th>
+                                         <th>Contact Name</th>
+                                         <th>Phone</th>
+                                         <th>Email</th>
+
+                                     </tr>
+                                 </thead>
+                                 <tbody>
+                                     <?php
+                                     
+                                     $q = 'SELECT tblagencies.* FROM `tblagencies` ';
+                                     $r = mysqli_query($dbc, $q);
+                                     if (mysqli_num_rows($r) >= 1) {
+                                         while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+                                             echo '<tr>
+                                                     <td>1</td>
+                                                     <td>' .
+                                                 $row['name'] .
+                                                 '</td>
+                                                     <td>' .
+                                                 $row['state'] .
+                                                 '</td>
+                                                     <td>' .
+                                                 $row['contact_person'] .
+                                                 '</td>';
+                                             // $role = $row['status']==1 ? "Checked" : "";
+                                             echo '<td>' .
+                                                 $row['phone'] .
+                                                 '</td>
+                                                     <td>' .
+                                                 $row['email'] .
+                                                 '</td>
+                                                 </tr>';
+                                         }
+                                     }
+                                     
+                                     ?>
+
+
+                                 </tbody>
+                                 <tfoot>
+                                     <tr>
+                                         <th>S/N</th>
+                                         <th>Agency Name</th>
+                                         <th>State</th>
+                                         <th>Contact Name</th>
+                                         <th>Phone</th>
+                                         <th>Email</th>
+                                     </tr>
+                                 </tfoot>
+                             </table>
+                         </div>
+                         <!-- /.card-body -->
+                     </div>
 
                  </div>
              </div>
